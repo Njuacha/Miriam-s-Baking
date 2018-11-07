a813a5d5464a74465f50452f72ef79e289592b34
Njuacha <betomis2006@yahoo.com>
2018-11-07 15:41:59 +0100

    Brought back the old exoplayer

--- app/src/main/java/com/example/android/miriamsbaking/fragment/StepFragment.java
+++ app/src/main/java/com/example/android/miriamsbaking/fragment/StepFragment.java
@@ -7,6 +7,7 @@ import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,13 +18,14 @@ import com.example.android.miriamsbaking.R;
 import com.example.android.miriamsbaking.model.Step;
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
-import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
@@ -46,7 +48,7 @@ public class StepFragment extends Fragment {
     @BindView(R.id.btn_previous)
     Button btnPrevious;
     @BindView(R.id.simple_exo_player_view)
-    PlayerView mPlayerView;
+    SimpleExoPlayerView mPlayerView;
 
     public interface StepFragListener{
         void onNextBtnClicked();
@@ -97,6 +99,7 @@ public class StepFragment extends Fragment {
 
            String videoUrl = ((Step)mSteps[mStepIndex]).getVideoURL();
            if(!(videoUrl.isEmpty()|| videoUrl == null)){
+               Log.d("videoUrl",videoUrl);
                setUpVideo(Uri.parse(videoUrl));
            }
 
@@ -122,7 +125,7 @@ public class StepFragment extends Fragment {
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
         if(mExoPlayer != null){
-            mContentPositon = mExoPlayer.getContentPosition();
+            mContentPositon = mExoPlayer.getCurrentPosition();
             outState.putLong(CONTENT_POSTION,mContentPositon);
         }
 
@@ -148,23 +151,19 @@ public class StepFragment extends Fragment {
     }
 
     public void setUpVideo(Uri videoUri){
-        
-        if(mExoPlayer != null){
-            // Create the player
-            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext()
-                    ,new DefaultTrackSelector(),new DefaultLoadControl());
-            // Bind player to view
+
+        if (mExoPlayer == null) {
+            // Create an instance of the ExoPlayer.
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
             mPlayerView.setPlayer(mExoPlayer);
-
-            // Produce DataSource instances through which media data is loaded.
-            DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(getContext(),
-                    Util.getUserAgent(getContext(), getString(R.string.app_name)));
-
-            MediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory)
-                    .createMediaSource(videoUri);
-            // Set the position so that any previous state can be resumed
+            // Prepare the MediaSource.
+            String userAgent = Util.getUserAgent(getContext(), getString(R.string.app_name));
+            MediaSource mediaSource = new ExtractorMediaSource(videoUri, new DefaultDataSourceFactory(
+                    getContext(), userAgent), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
             mExoPlayer.seekTo(mContentPositon);
-            //mExoPlayer.prepare(mediaSource);
             mExoPlayer.setPlayWhenReady(true);
         }
 
